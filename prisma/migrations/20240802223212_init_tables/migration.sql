8e5089c3f2543da63201d7425d58c1d4f965dc6d
RafaelEmery <rafael.emerycade@gmail.com>
2024-08-02 19:36:11 -0300

    feat: adding prisma and database models

---
+++ prisma/migrations/20240802223212_init_tables/migration.sql
@@ -0,0 +1,84 @@
+-- CreateEnum
+CREATE TYPE "TransactionStatus" AS ENUM ('in_progress', 'success', 'failed');
+
+-- CreateEnum
+CREATE TYPE "PayableStatus" AS ENUM ('waiting_funds', 'paid');
+
+-- CreateTable
+CREATE TABLE "Transaction" (
+    "id" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "method" TEXT NOT NULL,
+    "status" "TransactionStatus" NOT NULL DEFAULT 'in_progress',
+    "expectedFee" DOUBLE PRECISION,
+    "cardNumber" VARCHAR(16) NOT NULL,
+    "cardOwner" TEXT NOT NULL,
+    "cardExpirationYear" VARCHAR(4) NOT NULL,
+    "cardVerificationCode" VARCHAR(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Transaction_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Payable" (
+    "id" TEXT NOT NULL,
+    "transactionId" TEXT NOT NULL,
+    "customerId" TEXT NOT NULL,
+    "status" "PayableStatus" NOT NULL,
+    "paymentDate" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Payable_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Customer" (
+    "id" TEXT NOT NULL,
+    "description" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Customer_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Balance" (
+    "id" TEXT NOT NULL,
+    "customerId" TEXT NOT NULL,
+    "available" DOUBLE PRECISION NOT NULL DEFAULT 0.0,
+    "waitingFunds" DOUBLE PRECISION NOT NULL DEFAULT 0.0,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Balance_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "BalanceHistory" (
+    "id" TEXT NOT NULL,
+    "balanceId" TEXT NOT NULL,
+    "available" DOUBLE PRECISION NOT NULL DEFAULT 0.0,
+    "waitingFunds" DOUBLE PRECISION NOT NULL DEFAULT 0.0,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "BalanceHistory_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Payable_transactionId_key" ON "Payable"("transactionId");
+
+-- AddForeignKey
+ALTER TABLE "Payable" ADD CONSTRAINT "Payable_transactionId_fkey" FOREIGN KEY ("transactionId") REFERENCES "Transaction"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Payable" ADD CONSTRAINT "Payable_customerId_fkey" FOREIGN KEY ("customerId") REFERENCES "Customer"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Balance" ADD CONSTRAINT "Balance_customerId_fkey" FOREIGN KEY ("customerId") REFERENCES "Customer"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "BalanceHistory" ADD CONSTRAINT "BalanceHistory_balanceId_fkey" FOREIGN KEY ("balanceId") REFERENCES "Balance"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
