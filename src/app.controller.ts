423731c30789c6039026111eac3d3629f209bf8b
RafaelEmery <rafael.emerycade@gmail.com>
2024-08-02 18:31:40 -0300

    docs: adding info on hello endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('/hello')
+  @ApiTags('Health check')
+  @ApiOperation({ summary: `Get 'Hello World' message to health check 😎` })
+  @ApiResponse({
+    status: 200,
+    description: `'Hello World' message ✅`,
+  })
   getHello(): string {
     return this.appService.getHello();
   }
