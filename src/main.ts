e4e22ee6656b1c4027991e6f53eeb994665e5ae5
RafaelEmery <rafael.emerycade@gmail.com>
2024-08-02 18:18:07 -0300

    docs: adding swagger package and route

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { config } from 'dotenv';
+
+config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Simple PSP')
+    .setDescription(
+      'Simple Payment Service Provider (PSP) using NestJS to apply some concepts',
+    )
+    .setVersion('1.0')
+    .addTag('Payment Service Provider')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document, {
+    jsonDocumentUrl: 'docs/json',
+  });
+
+  await app.listen(process.env.APP_PORT);
 }
 bootstrap();
